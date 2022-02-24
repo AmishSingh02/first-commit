f118f6a25f87ee1a5814c975de9151bdcf019f2b
AmishSingh02 <amishsingh0203@gmail.com>
2022-02-24 15:34:44 +0530

    first commit

---
+++ Test1.java
@@ -0,0 +1,18 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.Statement;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		try {
+			Class.forName("org.postgrelsql.Driver");
+			System.out.println("driver class got loaded");
+			
+		
+		Connection con = 
+		// TODO Auto-generated method stub
+		}
+	}
+
+}
